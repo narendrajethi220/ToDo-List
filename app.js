f1a58771f4da9479daf859d512fbe0f3ec05a858
Narendra Jethi <narendrajethi220@gmail.com>
2023-11-27 11:07:05 +0530

    Completed Basic Structure and Logic.
    1) Structure of the To-Do List Application is completed.
    2) Added Logic of Adding new todo list using JavaScript.

---
+++ app.js
@@ -0,0 +1,22 @@
+const Express = require("express");
+const bodyParser = require("body-parser");
+
+var app = Express();
+
+app.set("view engine", "ejs");
+app.use(Express.urlencoded({ extended: true }));
+var items = [];
+var example = "Working";
+app.get("/", function (req, res) {
+  res.render("list", { ejes: items });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.ele1;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(8000, () => {
+  console.log("Server Started");
+});
